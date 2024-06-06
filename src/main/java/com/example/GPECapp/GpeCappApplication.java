cc2b95e8cc26e0378c46acaf7541ab9a10bba687
Kaszmirowski <Krzesikos@gmail.com>
2024-06-06 10:05:46 +0200

    inicjalizacja projektu

---
+++ src/main/java/com/example/GPECapp/GpeCappApplication.java
@@ -0,0 +1,13 @@
+package com.example.GPECapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpeCappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpeCappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GPECapp/GpeCappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GPECapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpeCappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
