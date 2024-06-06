b7035835cd48e46c399e56a87008f8d014d81047
Kaszmirowski <Krzesikos@gmail.com>
2024-06-06 10:21:22 +0200

    Add test controller

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,4 +1,12 @@
 package controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class HomeController {
+
+    @GetMapping("/home")
+    public String home() {
+
+        return "home";
+    }
 }
--- src/main/java/controller/PreviewController.java
+++ src/main/java/controller/PreviewController.java
@@ -1,4 +1,16 @@
 package controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Controller
 public class PreviewController {
+
+    @GetMapping("/preview")
+    public String preview() {
+
+        return "preview";
+    }
 }
