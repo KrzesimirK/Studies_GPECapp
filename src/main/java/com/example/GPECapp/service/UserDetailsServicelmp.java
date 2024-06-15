ce5617e913ec7fe3af1c2991493002f79fa8f35e
Kaszmirowski <Krzesikos@gmail.com>
2024-06-15 21:02:21 +0200

    powoli servis

---
+++ src/main/java/com/example/GPECapp/service/UserDetailsServicelmp.java
@@ -0,0 +1,21 @@
+package com.example.GPECapp.service;
+
+
+import com.example.GPECapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Service
+public class UserDetailsService implements  {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    public UserDetails
+
+
+
+
+}
